0c34947ad53f16555e1e70ce9d7a3c9f2c77b087
Abhi1482 <abhi2003jeet04@gmail.com>
2025-11-07 17:58:50 +0530

    added health check to server/urls

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from django.http import HttpResponse 
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
+path('', health_check),
 path('admin/', admin.site.urls),
 path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
